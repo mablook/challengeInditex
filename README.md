21653a7525fbf71329a08af931c329429f787174
mablook <mablook@gmail.com>
2023-02-14 12:31:15 +0000

    create persistent data lib

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 In the project directory, you can run:
 
-### `npm start`
+### `npm start <==> run development mode`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
@@ -14,12 +14,7 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.\
 You will also see any lint errors in the console.
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
+### `npm run build <==> production mode`
 
 Builds the app for production to the `build` folder.\
 It correctly bundles React in production mode and optimizes the build for the best performance.
@@ -39,8 +34,8 @@ Instead, it will copy all the configuration files and the transitive dependencie
 
 You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
 
-## Learn More
+## PersistentData <==> App Global State
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+The data is persistent in the browser, as required by the application. Taking into account the use of REACT the best practice is to access THE STATUS directly and not the client's BROWSER. Therefore, the data is saved in the local storage but loaded in the state of the application, CONTEXT global state
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+See the section about [Managing State](https://beta.reactjs.org/learn/managing-state) for more information.
--- src/context/podcastContext.tsx
+++ src/context/podcastContext.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Entry, PodcastContextType, PodcastDetails, PodcastDetailsResponse, RootFeed } from 'uiTypes';
 import { getData } from '../api/fetchApi';
+import { getLocalStorage } from '../api/persistData';
 
 
 export const PodcastContext = React.createContext<PodcastContextType | null>(null);
@@ -12,26 +13,22 @@ interface Props {
 const PodcastProvider: React.FC<Props> = ({ children }) => {
   const [podcasts, setPodcasts] = React.useState<RootFeed>();
   const [podcastDetail, setPodcastDetail] = React.useState<PodcastDetailsResponse>();
-  const local = localStorage.getItem("save-list")
 
   const getPodcasts = async () => {
-    if(!local && !podcasts){
-      const data = await getData(process.env.REACT_APP_API_BASE_URL || '')
-      localStorage.setItem("save-list", JSON.stringify(data));
+    if(!podcasts){
+      const data = await getLocalStorage({ url : process.env.REACT_APP_API_BASE_URL })
       await setPodcasts(data as RootFeed)
-    }else if(!podcasts && local){
-       await setPodcasts(JSON.parse(local))
     }
   }
+
   const getPodcastDetail = async (id:string) => {
-      let data:PodcastDetailsResponse = await getData(`${process.env.REACT_APP_API_PRODUCT_DETAIL}?id=${id}&media=podcast&entity=podcastEpisode&limit=15`)
-      if(data && local){
-        const info:Entry[] = (JSON.parse(local) as RootFeed).feed.entry.filter(obj => obj.id.attributes["im:id"] === id)!
+        if(!podcasts){
+        await getPodcasts()
+        }
+        let data:PodcastDetailsResponse = await getLocalStorage({ url :`${process.env.REACT_APP_API_PRODUCT_DETAIL}?id=${id}&media=podcast&entity=podcastEpisode&limit=15` })
+        const info:Entry[] = podcasts?.feed.entry.filter(obj => obj.id.attributes["im:id"] === id)!
         data.podcastInfo = info[0]
         await setPodcastDetail(data as PodcastDetailsResponse)
-     }else if(!local && !podcasts){
-        await getPodcasts()
-     }
   }
 
   return <PodcastContext.Provider value={{ podcasts, podcastDetail, getPodcasts, getPodcastDetail, setPodcastDetail }}>{children}</PodcastContext.Provider>;
