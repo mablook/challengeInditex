efab6b030011f67599628afa8251fa316d615d4f
mablook <mablook@gmail.com>
2023-02-14 11:13:50 +0000

    create lateral component

--- src/components/podcastDetails/podcastDetails.tsx
+++ src/components/podcastDetails/podcastDetails.tsx
@@ -1,9 +1,10 @@
 import { FC, useContext, useEffect, useCallback } from "react";
 import { Link, useParams } from "react-router-dom";
-import { Entry, PodcastContextType } from "uiTypes";
+import { PodcastContextType } from "uiTypes";
 import { PodcastContext } from "../../context/podcastContext";
 import { convertDate, convertTime } from "../../utils/date";
 import { cropText } from "../../utils/text";
+import LeftDetails from "../leftDetails/leftDetails";
 import styles from "./PodcastDetails.module.scss";
 
 const PodcastDetails: FC = () => {
@@ -11,45 +12,36 @@ const PodcastDetails: FC = () => {
   const { podcastDetail, getPodcastDetail } = useContext(PodcastContext) as PodcastContextType;
 
   const getDetailsInfo = useCallback(async () => {
-    podcastId && await getPodcastDetail(podcastId);
-  },[getPodcastDetail, podcastId])
+    podcastId && (await getPodcastDetail(podcastId));
+  }, [getPodcastDetail, podcastId]);
 
   useEffect(() => {
     if (podcastId && podcastId !== podcastDetail?.podcastInfo.id.attributes["im:id"]) {
-      getDetailsInfo()
+      getDetailsInfo();
     }
   }, [getDetailsInfo, podcastDetail?.podcastInfo.id.attributes, podcastId]);
 
   return (
     <div className={styles.container}>
-      <div className={[styles.card, styles.details].join(" ")}>
-        <div>
-          <img alt={podcastDetail?.podcastInfo["im:name"].label} src={podcastDetail?.podcastInfo["im:image"][2].label} className={styles.img} />
-        </div>
-        <hr className={styles.solid} />
-        <div className={styles.title}>
-          {podcastDetail?.podcastInfo["im:name"].label}
-          <div>by {podcastDetail?.podcastInfo["im:artist"].label}</div>
-        </div>
-        <hr className={styles.solid} />
-        <div className={styles.description}>
-          Description:
-          <div>{ cropText({ text : podcastDetail?.podcastInfo.summary.label, size: 100 })}</div>
-          </div>
-      </div>
+      <LeftDetails podcastImage={podcastDetail?.podcastInfo["im:image"][2].label} podcastName={podcastDetail?.podcastInfo["im:name"].label} podcastSummary={podcastDetail?.podcastInfo.summary.label} poscastArtist={podcastDetail?.podcastInfo["im:artist"].label} />
+
       <div>
         <div className={[styles.card, styles.episodes].join(" ")}>Episodes: {podcastDetail?.resultCount}</div>
         <ul className={[styles.card, styles.ul].join(" ")}>
-        <li key="li-header" className={styles.tr}>
-                  <div className={styles.tabletitle}>Title</div>
-                  <div className={styles.tabledate}>Date</div>
-                  <div className={styles.tableduration}>Duration</div>
-                </li>
+          <li key="li-header" className={styles.tr}>
+            <div className={styles.tabletitle}>Title</div>
+            <div className={styles.tabledate}>Date</div>
+            <div className={styles.tableduration}>Duration</div>
+          </li>
           {podcastDetail &&
             podcastDetail.results.map((results) => {
               return (
                 <li key={results.trackId} className={styles.tr}>
-                  <div className={styles.tabletitle}><Link to={`/podcast/${podcastDetail?.podcastInfo.id.attributes["im:id"]}/episode/${results.trackId}`} title={results.trackName}>{cropText({text:results.trackName, size:50})}</Link></div>
+                  <div className={styles.tabletitle}>
+                    <Link to={`/podcast/${podcastDetail?.podcastInfo.id.attributes["im:id"]}/episode/${results.trackId}`} title={results.trackName}>
+                      {cropText({ text: results.trackName, size: 50 })}
+                    </Link>
+                  </div>
                   <div className={styles.tabledate}>{convertDate({ date: results.releaseDate })}</div>
                   <div className={styles.tableduration}>{convertTime({ time: results.trackTimeMillis })}</div>
                 </li>
--- src/components/podcastEpisode/podcastEpisode.tsx
+++ src/components/podcastEpisode/podcastEpisode.tsx
@@ -4,58 +4,45 @@ import { Entry, PodcastContextType, PodcastDetails } from "uiTypes";
 import { PodcastContext } from "../../context/podcastContext";
 import { convertDate, convertTime } from "../../utils/date";
 import { cropText } from "../../utils/text";
+import LeftDetails from "../leftDetails/leftDetails";
 import styles from "./PodcastEpisode.module.scss";
 
 const PodcastEpisode: FC = () => {
   const { podcastId, episodeId } = useParams();
   const { podcastDetail, getPodcastDetail } = useContext(PodcastContext) as PodcastContextType;
-  const [ episode, setEpisode ] = useState<PodcastDetails>()
+  const [episode, setEpisode] = useState<PodcastDetails>();
 
-  console.log('--- the episode', episode)
+  console.log("--- the episode", episode);
 
   const getDetailsInfo = useCallback(async () => {
-    podcastId && await getPodcastDetail(podcastId);
-  },[getPodcastDetail, podcastId])
+    podcastId && (await getPodcastDetail(podcastId));
+  }, [getPodcastDetail, podcastId]);
 
   useEffect(() => {
     if (podcastId && podcastId !== podcastDetail?.podcastInfo.id.attributes["im:id"]) {
-      getDetailsInfo()
+      getDetailsInfo();
     }
   }, [getDetailsInfo, podcastDetail?.podcastInfo.id.attributes, podcastId]);
 
   useEffect(() => {
-    if(podcastDetail && episodeId){
-      const episode = podcastDetail.results.filter( e => e.trackId === Number(episodeId))
-      setEpisode(episode[0])
+    if (podcastDetail && episodeId) {
+      const episode = podcastDetail.results.filter((e) => e.trackId === Number(episodeId));
+      setEpisode(episode[0]);
     }
-  },[episodeId])
+  }, [episodeId]);
 
   return (
     <div className={styles.container}>
-      <div className={[styles.card, styles.details].join(" ")}>
-        <div>
-          <img alt={podcastDetail?.podcastInfo["im:name"].label} src={podcastDetail?.podcastInfo["im:image"][2].label} className={styles.img} />
-        </div>
-        <hr className={styles.solid} />
-        <div className={styles.title}>
-          {podcastDetail?.podcastInfo["im:name"].label}
-          <div>by {podcastDetail?.podcastInfo["im:artist"].label}</div>
-        </div>
-        <hr className={styles.solid} />
-        <div className={styles.description}>
-          Description:
-          <div>{ cropText({ text : podcastDetail?.podcastInfo.summary.label, size: 100 })}</div>
-          </div>
-      </div>
-      <div className={[styles.card, styles.details].join(" ")}>
-    <div>{episode?.trackName}</div>
-    <div>{episode?.description}</div>
-    <div>
+      <LeftDetails podcastImage={podcastDetail?.podcastInfo["im:image"][2].label} podcastName={podcastDetail?.podcastInfo["im:name"].label} podcastSummary={podcastDetail?.podcastInfo.summary.label} poscastArtist={podcastDetail?.podcastInfo["im:artist"].label} />
 
-   <audio controlsList="nodownload" controls>
-     <source src={episode?.episodeUrl} type="audio/mpeg" />
-    </audio>
-    </div>
+      <div className={[styles.card, styles.details].join(" ")}>
+        <div>{episode?.trackName}</div>
+        <div>{episode?.description}</div>
+        <div>
+          <audio controlsList="nodownload" controls>
+            <source src={episode?.episodeUrl} type="audio/mpeg" />
+          </audio>
+        </div>
       </div>
     </div>
   );
